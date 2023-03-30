a0a719265b4a8d49728f391293a001355c6f956b
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-03-30 10:49:41 +0900

    feat : TestGame으로부터 쉐이더를 업데이트(포그가 적용된 쉐이더)

--- Do-Not-Die/HLSL/SkySphereVS.hlsl
+++ Do-Not-Die/HLSL/SkySphereVS.hlsl
@@ -1,18 +1,12 @@
 #include "include/VertexCommon.hlsli"
 
-cbuffer cb_data : register(b1)
-{
-    matrix world_matrix;
-};
-
 struct VS_IN
 {
-    float3 p : F3_POSITION;  
+    float3 p : F3_POSITION;
     float3 n : F3_NORMAL;
-    float4 c : F4_COLOR;
     float2 t : F2_TEXTURE;
 };
-struct VS_OUT  
+struct VS_OUT
 {
     float4 p : SV_POSITION;
     float4 n : NORMAL;
@@ -27,10 +21,10 @@ VS_OUT VS(VS_IN input)
     float4 local = float4(input.p, 1.0f);
     float4 normal = float4(input.n, 1.0f);
     
-    float4 world = mul(local, world_matrix);  
+    float4 world = mul(local, FarScaleMatrix() * camera_translation);
     float4 projection = mul(world, ViewProjection());
 
-    output.origin = input.p;
+    output.origin = world;
     
     output.p = projection;
     output.n = normal;
