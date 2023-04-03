8f4e8f074509c7ae5368996f7ab7c5c0200e91b7
EussyKR <hang3137@naver.com>
2023-04-03 15:46:20 +0900

    feat : Muzzle, Explosion Effect 추가

---
+++ Do-Not-Die/src/FX_Explosion.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "FX_Explosion.h"
+#include "SceneMgr.h"
+#include "ResourceMgr.h"
+#include "EffectMgr.h"
+
+using namespace reality;
+
+void FX_Explosion::OnInit(entt::registry& registry)
+{
+	FX_BaseEffectActor::OnInit(registry);
+
+	AddEffectComponent("explosion_effect", 5.0f);
+
+	AddPointLightComponent("PL_Explosion", 1.0f);
+}
