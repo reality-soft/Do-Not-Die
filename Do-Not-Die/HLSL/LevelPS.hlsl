c1b5e39f66da9370fdc15f39ba5cb087e92f3126
EussyKR <hang3137@naver.com>
2023-04-04 14:15:56 +0900

    chore : 포인트 조명, 스포트라이트 조명 밝기 조정

--- Do-Not-Die/HLSL/LevelPS.hlsl
+++ Do-Not-Die/HLSL/LevelPS.hlsl
@@ -31,8 +31,8 @@ float4 PS(PS_OUT input) : SV_Target
     albedo = ApplyHemisphericAmbient(input.n, albedo);
     
     final_color = ApplyCookTorrance(albedo, roughness.r, specular_strength, input.n, input.view_dir);
-    final_color += ApplyPointLight(middle_albedo, input.n, input.origin, input.view_dir);
-    final_color += ApplySpotLight(middle_albedo, input.n, input.origin, input.view_dir);
+    final_color += ApplyPointLight(WhiteColor(), input.n, input.origin, input.view_dir);
+    final_color += ApplySpotLight(WhiteColor(), input.n, input.origin, input.view_dir);
 
     final_color = ApplyDistanceFog(final_color, input.origin);
 
