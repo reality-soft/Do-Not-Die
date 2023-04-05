6ece324f0a8cdd141205847f39a472dd65ace53c
EussyKR <hang3137@naver.com>
2023-04-05 16:38:27 +0900

    feat : 인게임 동적 UI 텍스트 추가

--- Do-Not-Die/src/UI/InGameScene/UI_Actor_Ingame.cpp
+++ Do-Not-Die/src/UI/InGameScene/UI_Actor_Ingame.cpp
@@ -25,9 +25,9 @@ void UI_Actor_Ingame::CreateUI()
 	weapon_ui_ = make_shared<UI_Image>();
 	weapon_ui_->InitImage("T_Handgun_01.png");
 	weapon_ui_->SetLocalRectByMin({ 100.0f, ENGINE->GetWindowSize().y - 200.0f }, 512.0f, 179.0f);
-	auto ammo_text = make_shared<UI_Text>();
-	weapon_ui_->AddChildUI(ammo_text);
-	ammo_text->InitText("30", BASIC, { 320.0f, 72.0f }, 2.0f);
+	ammo_text_ = make_shared<UI_Text>();
+	weapon_ui_->AddChildUI(ammo_text_);
+	ammo_text_->InitText("30", BASIC, { 320.0f, 72.0f }, 1.5f);
 	ui_comp.ui_list.insert({ "Weapon UI", weapon_ui_ });
 
 	// 상태 + 아이템 UI
@@ -57,12 +57,12 @@ void UI_Actor_Ingame::CreateUI()
 	time_ui_->InitImage("T_TimeUI.png");
 	time_ui_->SetLocalRectByMin({ 0, 0 }, 300.0f, 100.0f);
 
-	auto time_text = make_shared<UI_Text>();
-	time_text->InitText("10", E_Font::ROTUNDA, { 200.0f, 30.0f });
-	time_ui_->AddChildUI(time_text);
-	auto kill_text = make_shared<UI_Text>();
-	kill_text->InitText("300", E_Font::ROTUNDA, { 120.0f, 70.0f });
-	time_ui_->AddChildUI(kill_text);
+	time_text_ = make_shared<UI_Text>();
+	time_text_->InitText("10", E_Font::ROTUNDA, { 190.0f, 22.0f });
+	time_ui_->AddChildUI(time_text_);
+	kill_text_ = make_shared<UI_Text>();
+	kill_text_->InitText("300", E_Font::ROTUNDA, { 120.0f, 65.0f });
+	time_ui_->AddChildUI(kill_text_);
 
 	ui_comp.ui_list.insert({ "time UI", time_ui_ });
 
@@ -71,6 +71,15 @@ void UI_Actor_Ingame::CreateUI()
 	objective_ui_ = make_shared<UI_Image>();
 	objective_ui_->InitImage("T_ObjectiveUI.png");
 	objective_ui_->SetLocalRectByMin({ ENGINE->GetWindowSize().x * 4.0f / 5.0f, ENGINE->GetWindowSize().y * 4.0f / 10.0f }, 323.0f, 387.0f);
+
+	tire_text_ = make_shared<UI_Text>();
+	tire_text_->InitText("TIRE : 10 / 30", E_Font::ROTUNDA, { 50.0f, 140.0f });
+	objective_ui_->AddChildUI(tire_text_);
+
+	steel_text_ = make_shared<UI_Text>();
+	steel_text_->InitText("STEEL : 10 / 30", E_Font::ROTUNDA, { 50.0f, 200.0f });
+	objective_ui_->AddChildUI(steel_text_);
+
 	ui_comp.ui_list.insert({ "Objective UI", objective_ui_ });
 
 	// CrossHair UI
@@ -91,5 +100,27 @@ void UI_Actor_Ingame::UpdateUI()
 		hp_img_->render_data_.tex_id = "T_HpBarRed.png";
 
 	hp_img_->SetLocalRectByMin({ status_ui->rect_transform_.world_rect.width / 2.0f - 200.0f, 150.0f }, 400.0f * hp_ratio, 30.0f);
+	
+	// Time Update
+	static float time = 1000.0f;
+	time -= TIMER->GetDeltaTime();
+	string time_str = to_string(time);
+	time_str = time_str.substr(0, time_str.find('.') + 2 + 1);
+	time_text_->SetText(time_str);
+
+	// Kill Update
+	static int kill = 0;
+	kill = TIMER->game_time;
+	kill_text_->SetText(to_string(kill));
+
+	// Vehicle Item Update
+	string tire_str = "Tire : ";
+	string tire_count = "10";
+	string tire_max_count = " / 30";
+	tire_text_->SetText(tire_str + tire_count + tire_max_count);
+	string steel_str = "Steel : ";
+	string steel_count = "10";
+	string steel_max_count = " / 30";
+	steel_text_->SetText(steel_str + steel_count + steel_max_count);
 }
 
--- Do-Not-Die/src/UI/InGameScene/UI_Actor_Ingame.h
+++ Do-Not-Die/src/UI/InGameScene/UI_Actor_Ingame.h
@@ -14,10 +14,15 @@ namespace reality
 		Player* player_;
 	private:
 		shared_ptr<UI_Image>	weapon_ui_;
+			shared_ptr<UI_Text>		ammo_text_;
 		shared_ptr<UI_Minimap>	minimap_ui;
 		shared_ptr<UI_Image>	status_ui;
 		shared_ptr<UI_Image>	objective_ui_;
+			shared_ptr<UI_Text>		tire_text_;
+			shared_ptr<UI_Text>		steel_text_;
 		shared_ptr<UI_Image>	time_ui_;
+			shared_ptr<UI_Text>		time_text_;
+			shared_ptr<UI_Text>		kill_text_;
 		shared_ptr<UI_Image>	crosshair_ui_;
 		// Need to be Updated UI
 	private:
