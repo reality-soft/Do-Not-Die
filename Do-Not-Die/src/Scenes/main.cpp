1d2d87e74b982cf938c375db87789e8c3bc3bfc9
EussyKR <hang3137@naver.com>
2023-04-06 13:47:32 +0900

    refactor : 게임 시작 시 스타트 신만 추가하고 스타트 신으로 시작하도록 수정

--- Do-Not-Die/src/Scenes/main.cpp
+++ Do-Not-Die/src/Scenes/main.cpp
@@ -1,12 +1,13 @@
-#include "InGameScene.h"
 #include "StartScene.h"
+#include "LoadingScene.h"
+#include "InGameScene.h"
 
 int WINAPI wWinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
 	ENGINE->OnInit(hinstance, L"Game-Engine", { 1920, 1080 });
 
-	SCENE_MGR->SetScene<StartScene>();
-	//SCENE_MGR->SetScene<InGameScene>();
+	SCENE_MGR->AddScene<StartScene>(E_SceneType::START);
+	SCENE_MGR->ChangeScene(E_SceneType::START);
 	ENGINE->Run();
 
 	ENGINE->OnRelease();
