fdfab636078cf755d7ae9f93726aa7b4924fe467
EussyKR <hang3137@naver.com>
2023-04-06 13:43:02 +0900

    feat : 스타트 씬 클래스 간소화 및 옵션 창 추가

--- Do-Not-Die/src/UI/StartScene/UI_Actor_StartScene.cpp
+++ Do-Not-Die/src/UI/StartScene/UI_Actor_StartScene.cpp
@@ -1,11 +1,7 @@
 #include "stdafx.h"
 #include "UI_Actor_StartScene.h"
-#include "UI_Button_Newgame.h"
-#include "UI_Button_Loadgame.h"
-#include "UI_Button_Option.h"
-#include "UI_Button_Exit.h"
-#include "Engine.h"
 #include "SceneMgr.h"
+#include "InGameScene.h"
 
 using namespace reality;
 
@@ -24,7 +20,7 @@ void UI_Actor_StartScene::OnUpdate()
 
 void UI_Actor_StartScene::CreateUI()
 {
-	C_UI& ui_comp = reg_scene_->get<C_UI>(GetEntityId());
+	ui_comp_ = &reg_scene_->get<C_UI>(GetEntityId());
 
 	float window_size_width = ENGINE->GetWindowSize().x;
 	float window_size_height = ENGINE->GetWindowSize().y;
@@ -33,35 +29,105 @@ void UI_Actor_StartScene::CreateUI()
 	background_ = make_shared<UI_Image>();
 	background_->InitImage("T_StartScene_background.png");
 	background_->SetLocalRectByMin({ 0, 0 }, window_size_width, window_size_height);
-	ui_comp.ui_list.insert({ "Background", background_ });
+	ui_comp_->ui_list.insert({ "Background", background_ });
 	
 	// New Game Button
-	newgame_button_ = make_shared<UI_Button_Newgame>();
+	newgame_button_ = make_shared<UI_Button>();
 	newgame_button_->InitButton("T_Button_NewGame_Normal.png", "T_Button_NewGame_Hover.png", "T_Button_NewGame_Pushed.png");
 	newgame_button_->SetLocalRectByMin({ window_size_width * 3.0f / 4.0f, window_size_height * 3.0f / 5.0f }, 200.0f, 70.0f);
-	ui_comp.ui_list.insert({ "New Game Button", newgame_button_ });
+	ui_comp_->ui_list.insert({ "New Game Button", newgame_button_ });
 
 	// Load Game Button
-	loadinggame_button = make_shared<UI_Button_Loadgame>();
+	loadinggame_button = make_shared<UI_Button>();
 	loadinggame_button->InitButton("T_Button_LoadGame_Normal.png", "T_Button_LoadGame_Hover.png", "T_Button_LoadGame_Pushed.png");
 	loadinggame_button->SetLocalRectByMin({ window_size_width * 3.0f / 4.0f, window_size_height * 3.0f / 5.0f + 70.0f * 1.0f }, 200.0f, 70.0f);
-	ui_comp.ui_list.insert({ "Load Game Button", loadinggame_button });
+	ui_comp_->ui_list.insert({ "Load Game Button", loadinggame_button });
 
 	// Option Button
-	option_button_ = make_shared<UI_Button_Option>();
+	option_button_ = make_shared<UI_Button>();
 	option_button_->InitButton("T_Button_Option_Normal.png", "T_Button_Option_Hover.png", "T_Button_Option_Pushed.png");
 	option_button_->SetLocalRectByMin({ window_size_width * 3.0f / 4.0f, window_size_height * 3.0f / 5.0f + 70.0f * 2.0f }, 200.0f, 70.0f);
-	ui_comp.ui_list.insert({ "Option Button", option_button_ });
+	ui_comp_->ui_list.insert({ "Option Button", option_button_ });
 
 	// Exit Button
-	exit_button_ = make_shared<UI_Button_Exit>();
+	exit_button_ = make_shared<UI_Button>();
 	exit_button_->InitButton("T_Button_Exit_Normal.png", "T_Button_Exit_Hover.png", "T_Button_Exit_Pushed.png");
 	exit_button_->SetLocalRectByMin({ window_size_width * 3.0f / 4.0f, window_size_height * 3.0f / 5.0f + 70.0f * 3.0f }, 200.0f, 70.0f);
-	ui_comp.ui_list.insert({ "Exit Button", exit_button_ });
+	ui_comp_->ui_list.insert({ "Exit Button", exit_button_ });
+
+	// Opaque
+	option_opaque_ = make_shared<UI_Image>();
+	option_opaque_->InitImage("T_Opaque.png");
+	option_opaque_->SetLocalRectByMin({ 0.0f, 0.0f }, window_size_width, window_size_height);
+
+	// Option Window
+	option_window_ = make_shared<UI_Image>();
+	option_window_->InitImage("T_Option.png");
+	option_opaque_->AddChildUI(option_window_);
+	option_window_->SetLocalRectByCenter({ window_size_width / 2.0f, window_size_height / 2.0f }, 246.0f * 2.0f, 305.0f * 2.0f);
+
+	// Option Close Button
+	option_close_button_ = make_shared<UI_Button>();
+	option_close_button_->InitButton("T_Button_Close_Normal.png", "T_Button_Close_Hover.png", "T_Button_Close_Normal.png");
+	option_window_->AddChildUI(option_close_button_);
+	option_close_button_->SetLocalRectByCenter({ 370.0f, 80.0f }, 51.0f, 56.0f);
+
 }
 
 void UI_Actor_StartScene::UpdateUI()
 {
+	// When NewGame Button Selected
+	if (newgame_button_->GetCurrentState() == E_UIState::UI_SELECT)
+	{
+		thread loading_thread = thread(&UI_Actor_StartScene::LoadingIngameScene, this);
+		loading_thread.detach();
+		SCENE_MGR->ChangeScene(E_SceneType::LOADING);
+	}
 
+	// When exit Button Selected
+	if (exit_button_->GetCurrentState() == E_UIState::UI_SELECT)
+	{
+		DestroyWindow(ENGINE->GetWindowHandle());
+	}
+
+	// When Option Button Selected
+	if (option_button_->GetCurrentState() == E_UIState::UI_SELECT)
+		OpenOptionWindow();
+
+	// If Option Window Opened
+	if (ui_comp_->ui_list.find("Option Window") != ui_comp_->ui_list.end())
+	{
+		if (option_close_button_->GetCurrentState() == E_UIState::UI_SELECT || DINPUT->GetKeyState(DIK_ESCAPE) == KEY_PUSH)
+			CloseOptionWindow();
+	}
 }
 
+void UI_Actor_StartScene::OpenOptionWindow()
+{
+	newgame_button_->Off();
+	loadinggame_button->Off();
+	option_button_->Off();
+	exit_button_->Off();
+
+	option_button_->SetCurrentState(E_UIState::UI_NORMAL);
+
+	ui_comp_->ui_list.insert({ "Option Window", option_opaque_ });
+}
+
+void UI_Actor_StartScene::CloseOptionWindow()
+{
+	newgame_button_->On();
+	loadinggame_button->On();
+	option_button_->On();
+	exit_button_->On();
+
+	option_close_button_->SetCurrentState(E_UIState::UI_NORMAL);
+
+	ui_comp_->ui_list.erase("Option Window");
+}
+
+
+void UI_Actor_StartScene::LoadingIngameScene()
+{
+	SCENE_MGR->AddScene<InGameScene>(INGAME);
+}
--- Do-Not-Die/src/UI/StartScene/UI_Actor_StartScene.h
+++ Do-Not-Die/src/UI/StartScene/UI_Actor_StartScene.h
@@ -9,14 +9,25 @@ namespace reality
 		virtual void OnInit(entt::registry& registry);
 		virtual void OnUpdate();
 	private:
+		C_UI* ui_comp_;
+	private:
 		shared_ptr<UI_Image>	background_;
 		shared_ptr<UI_Button>	newgame_button_;
 		shared_ptr<UI_Button>	loadinggame_button;
 		shared_ptr<UI_Button>	option_button_;
 		shared_ptr<UI_Button>	exit_button_;
+
+		shared_ptr<UI_Image>	option_opaque_;
+		shared_ptr<UI_Image>	option_window_;
+		shared_ptr<UI_Button>	option_close_button_;
 	private:
 		void CreateUI();
 		void UpdateUI();
+	private:
+		void OpenOptionWindow();
+		void CloseOptionWindow();
+	private:
+		void LoadingIngameScene();
 	};
 
 }
