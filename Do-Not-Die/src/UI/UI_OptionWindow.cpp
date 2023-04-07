1220467e619f438d180429e3554308c2a366042b
EussyKR <hang3137@naver.com>
2023-04-07 19:13:12 +0900

    feat : 화면 해상도 리스트 박스 구현

--- Do-Not-Die/src/UI/UI_OptionWindow.cpp
+++ Do-Not-Die/src/UI/UI_OptionWindow.cpp
@@ -25,11 +25,23 @@ void UI_OptionWindow::InitOptionWindow()
 	option_window_->AddChildUI(option_close_button_);
 	option_close_button_->SetLocalRectByCenter({ 370.0f, 80.0f }, 51.0f, 56.0f);
 
+	// Resolution Listbox
+	option_resolution_list_box_ = make_shared<UI_Listbox>();
+	option_resolution_list_box_->InitListBox("T_Listbox_OpenPanel.png", "T_Listbox_Closed.png", "T_Listbox_Opened.png");
+	option_resolution_list_box_->InitItemButton({ 35.0f, 35.0f }, 200.0f, 30.0f, 40.0f, "T_Listbox_Item_Normal.png", "T_Listbox_Item_Hover.png", "T_Listbox_Item_Normal.png");
+	option_window_->AddChildUI(option_resolution_list_box_);
+	option_resolution_list_box_->SetLocalRectByMin({ 40.0f, 180.0f }, 265.0f, 51.0f);
+	option_resolution_list_box_->SetOpenPanelLocalRectByMin({ 0.0f, option_resolution_list_box_->rect_transform_.world_rect.height - 20.0f }, 263.0f, 181.0f);
+	option_resolution_list_box_->AddItem("1920 x 1080");
+	option_resolution_list_box_->AddItem("800 x 600");
+	option_resolution_list_box_->SetItemSelected(0);
+
+
 	// Volume Slider
 	option_volume_slider_ = make_shared<UI_Slider>();
 	option_volume_slider_->InitSlider("T_Volume_Slider_back.png", "T_Volume_Slider.png", "T_Volume_Slider.png");
 	option_window_->AddChildUI(option_volume_slider_);
-	option_volume_slider_->SetLocalRectByMin({50.0f, 466.0f}, 265.0f, 51.0f);
+	option_volume_slider_->SetLocalRectByMin({50.0f, 430.0f}, 265.0f, 51.0f);
 	option_volume_slider_->SetSliderLocalRect(44.0f, 18.0f);
 }
 
--- Do-Not-Die/src/UI/UI_OptionWindow.h
+++ Do-Not-Die/src/UI/UI_OptionWindow.h
@@ -2,6 +2,7 @@
 #include "UI_Image.h"
 #include "UI_Button.h"
 #include "UI_Slider.h"
+#include "UI_Listbox.h"
 
 namespace reality
 {
@@ -14,6 +15,7 @@ namespace reality
 	private:
 		shared_ptr<UI_Image>	option_window_;
 		shared_ptr<UI_Button>	option_close_button_;
+		shared_ptr<UI_Listbox>  option_resolution_list_box_;
 		shared_ptr<UI_Slider>	option_volume_slider_;
 	public:
 		E_UIState	GetCloseButtonState();
