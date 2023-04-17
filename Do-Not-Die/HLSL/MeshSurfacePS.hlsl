03a6e88ec33cfe5ca44f33a31efdbb3b1dd4f96a
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-04-17 17:27:21 +0900

    feat: 스태틱 메시가 노말 텍스쳐도 활용할 수 있도록 수정

--- Do-Not-Die/HLSL/MeshSurfacePS.hlsl
+++ Do-Not-Die/HLSL/MeshSurfacePS.hlsl
@@ -6,6 +6,8 @@ struct PS_OUT
     float3 n : NORMAL0;
     float2 t : TEXCOORD;
     
+    matrix normal_transform : NORMALMATRIX;
+
     float lod : TEXCOORD1;
     float3 view_dir : TEXCOORD2;
     float3 origin : NORMAL1;
@@ -19,14 +21,21 @@ float4 PS(PS_OUT input) : SV_Target
     float4 albedo = textures[0].Sample(samper_state, input.t);
     float4 final_color = WhiteColor();
     float4 roughness = textures[3].Sample(samper_state, input.t);
+    
+    float4 normal = textures[3].Sample(samper_state, input.t);
+    if (length(saturate(normal)) <= EPSILON) {
+        normal = float4(input.n, 0.0f);
+    }
+
+    normal = mul(normal, input.normal_transform);
 
     albedo = ChangeSaturation(albedo, 1.3f);
     albedo = ChangeValue(albedo, 0.5f);
-    albedo = ApplyHemisphericAmbient(input.n, albedo);
+    albedo = ApplyHemisphericAmbient(normal.xyz, albedo);
     
-    final_color = ApplyCookTorrance(albedo, 0.2f, specular_strength, input.n, input.view_dir);
-    final_color += ApplyPointLight(WhiteColor(), input.n, input.origin, input.view_dir);
-    final_color += ApplySpotLight(WhiteColor(), input.n, input.origin, input.view_dir);
+    final_color = ApplyCookTorrance(albedo, 0.2f, specular_strength, normal.xyz, input.view_dir);
+    final_color += ApplyPointLight(WhiteColor(), normal.xyz, input.origin, input.view_dir);
+    final_color += ApplySpotLight(WhiteColor(), normal.xyz, input.origin, input.view_dir);
     final_color = ApplyDistanceFog(final_color, input.origin);
     
     return final_color;
--- Do-Not-Die/HLSL/StaticMeshVS.hlsl
+++ Do-Not-Die/HLSL/StaticMeshVS.hlsl
@@ -20,6 +20,8 @@ struct VS_OUT
     float4 p : SV_POSITION;
     float3 n : NORMAL0;
     float2 t : TEXCOORD0;
+
+    matrix normal_transform : NORMALMATRIX;
     
     float lod : TEXCOORD1;
     float3 view_dir : TEXCOORD2;
@@ -36,11 +38,7 @@ VS_OUT VS(VS_IN input)
     float4 animated_local = mul(local, animation_matrix);
     animated_local = mul(animated_local, owner_local);
 
-    float4 normal = float4(input.n, 1.0f);
-    normal = mul(normal, local_matrix);
-    normal = mul(normal, local_offset);
-    float4 animated_normal = mul(normal, animation_matrix);
-    animated_normal = mul(animated_normal, owner_local);
+    output.normal_transform = mul(owner_local, animation_matrix);
 
     float4 world = mul(animated_local, world_matrix);
     float4 projection = mul(world, ViewProjection());
@@ -48,9 +46,9 @@ VS_OUT VS(VS_IN input)
     output.lod = GetLod(input.p);
 
     output.p = projection;
-    output.n = animated_normal;  
+    output.n = input.n;
     output.t = input.t;
-    
+
     output.lod = GetLod(input.p);
     output.view_dir = normalize(camera_world - world).xyz;
     output.origin = world;
