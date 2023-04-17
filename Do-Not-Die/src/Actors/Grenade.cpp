ae8d3c245e906aeeca9b0b9003f22e8929487080
Hwang Su Jin <hang3137@naver.com>
2023-04-17 16:49:38 +0900

    feat : Grenade에 C_SphereCollision 추가

--- Do-Not-Die/src/Actors/Grenade.cpp
+++ Do-Not-Die/src/Actors/Grenade.cpp
@@ -6,6 +6,10 @@ void Grenade::OnInit(entt::registry& registry)
 {
 	Actor::OnInit(registry);
 
+	reality::C_SphereCollision sphere_comp;
+	sphere_comp.SetSphereData(XMFLOAT3(0, 0, 0), 5);
+	registry.emplace_or_replace<reality::C_SphereCollision>(entity_id_, sphere_comp);
+
 	reality::C_StaticMesh stm;
 	stm.local = XMMatrixIdentity();
 	stm.world = XMMatrixIdentity();
@@ -13,7 +17,11 @@ void Grenade::OnInit(entt::registry& registry)
 	stm.vertex_shader_id = "StaticMeshVS.cso";
 	registry.emplace_or_replace<reality::C_StaticMesh>(entity_id_, stm);
 
-	transform_tree_.root_node = make_shared<TransformTreeNode>(TYPE_ID(C_StaticMesh));
+	QUADTREE->RegistDynamicSphere(GetEntityId());
+
+	transform_tree_.root_node = make_shared<TransformTreeNode>(TYPE_ID(C_SphereCollision));
+	transform_tree_.AddNodeToNode(TYPE_ID(C_SphereCollision), TYPE_ID(C_StaticMesh));
+
 	transform_tree_.root_node->OnUpdate(registry, entity_id_, transform_matrix_);
 }
 
@@ -28,6 +36,12 @@ void Grenade::OnUpdate()
 
 	transform_matrix_ = XMMatrixTranslationFromVector(current_pos);
 	transform_tree_.root_node->OnUpdate(*reg_scene_, entity_id_, transform_matrix_);
+
+	auto& sphere_comp = reg_scene_->get<C_SphereCollision>(GetEntityId());
+	if (sphere_comp.is_collide)
+	{
+		int i = 0;
+	}
 }
 
 
