4723be37b079fc36e5d0bc592cd3f616c1d0237c
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-04-19 14:46:19 +0900

    feat : Animation State 수정

--- Do-Not-Die/src/Actors/Player.h
+++ Do-Not-Die/src/Actors/Player.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Engine_include.h"
 #include "AnimationStateMachine.h"
+#include "AnimationState.h"
 #include "Item.h"
 
 #define INVENTORY_MAX 4
@@ -85,7 +86,7 @@ public:
 		states_.insert({ BASE_POSE, make_shared<BasePose>() });
 		states_.insert({ AIM_POSE, make_shared<AimPose>()});
 		states_.insert({ FIRE, make_shared<Fire>()});
-		transitions_.insert({ BASE_POSE, Transition(AIM_POSE,[this](const AnimationBase* animation_base) {
+		transitions_.insert({ BASE_POSE, Transition(AIM_POSE,[this](const AnimationStateMachine* animation_base) {
 				Player* player = SCENE_MGR->GetActor<Player>(owner_id_);
 				if (player->IsAiming()) {
 					return true;
@@ -95,7 +96,7 @@ public:
 				}
 			})
 		});
-		transitions_.insert({ AIM_POSE, Transition(BASE_POSE,[this](const AnimationBase* animation_base) {
+		transitions_.insert({ AIM_POSE, Transition(BASE_POSE,[this](const AnimationStateMachine* animation_base) {
 				Player* player = SCENE_MGR->GetActor<Player>(owner_id_);
 				if (player->IsAiming() == false) {
 					return true;
@@ -105,7 +106,7 @@ public:
 				}
 			})
 		});
-		transitions_.insert({ AIM_POSE, Transition(FIRE,[this](const AnimationBase* animation_base) {
+		transitions_.insert({ AIM_POSE, Transition(FIRE,[this](const AnimationStateMachine* animation_base) {
 				Player* player = SCENE_MGR->GetActor<Player>(owner_id_);
 				if (player->fire_ == true) {
 					return true;
@@ -115,7 +116,7 @@ public:
 				}
 			})
 		});
-		transitions_.insert({ FIRE, Transition(AIM_POSE,[this](const AnimationBase* animation_base) {
+		transitions_.insert({ FIRE, Transition(AIM_POSE,[this](const AnimationStateMachine* animation_base) {
 				Player* player = SCENE_MGR->GetActor<Player>(owner_id_);
 				if (this->IsAnimationEnded()) {
 					player->fire_ = false;
@@ -138,15 +139,15 @@ public:
 	public:
 		BasePose() : AnimationState(BASE_POSE) {}
 	public:
-		virtual void Enter(AnimationBase* animation_base) override
+		virtual void Enter(AnimationStateMachine* animation_base) override
 		{
 			animation_base->SetAnimation("", 0.2f);
 		}
-		virtual void Exit(AnimationBase* animation_base) override
+		virtual void Exit(AnimationStateMachine* animation_base) override
 		{
 		}
 
-		virtual void OnUpdate(AnimationBase* animation_base) override
+		virtual void OnUpdate(AnimationStateMachine* animation_base) override
 		{
 		}
 	};
@@ -155,14 +156,14 @@ public:
 	public:
 		AimPose() : AnimationState(AIM_POSE) {}
 	public:
-		virtual void Enter(AnimationBase* animation_base) override
+		virtual void Enter(AnimationStateMachine* animation_base) override
 		{
 			animation_base->SetAnimation("A_TP_CH_Handgun_Aim_Pose_Retargeted_Unreal Take.anim", 0.2f);
 		}
-		virtual void Exit(AnimationBase* animation_base) override
+		virtual void Exit(AnimationStateMachine* animation_base) override
 		{
 		}
-		virtual void OnUpdate(AnimationBase* animation_base) override
+		virtual void OnUpdate(AnimationStateMachine* animation_base) override
 		{
 		}
 	};
@@ -171,14 +172,14 @@ public:
 	public:
 		Fire() : AnimationState(FIRE) {}
 	public:
-		virtual void Enter(AnimationBase* animation_base) override
+		virtual void Enter(AnimationStateMachine* animation_base) override
 		{
 			animation_base->SetAnimation("A_TP_CH_Handgun_Fire_Retargeted_Unreal Take.anim", 0.0f);
 		}
-		virtual void Exit(AnimationBase* animation_base) override
+		virtual void Exit(AnimationStateMachine* animation_base) override
 		{
 		}
-		virtual void OnUpdate(AnimationBase* animation_base) override
+		virtual void OnUpdate(AnimationStateMachine* animation_base) override
 		{
 		}
 	};
