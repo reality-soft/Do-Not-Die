1971f4c4a2c6fd08a746bd7b45d2e7ded9f942d4
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-04-19 21:18:42 +0900

    feat : 자동차의 본네트마다 트리거 생성

--- Do-Not-Die/src/Game/WaveSystem.cpp
+++ Do-Not-Die/src/Game/WaveSystem.cpp
@@ -1,10 +1,14 @@
 #include "WaveSystem.h"
+#include "Player.h"
 #include "Item.h"
 
 void reality::WaveSystem::OnCreate(entt::registry& reg)
 {
 	QUADTREE->ImportGuideLines("ItemSpawn.mapdat", GuideType::eSpawnPoint);
-	item_spawns_ = QUADTREE->GetGuideLines("ItemSpawn");
+	QUADTREE->ImportGuideLines("DND_RepairPart_1.mapdat", GuideType::eSpawnPoint);
+
+	item_spawns_ = QUADTREE->GetGuideLines("ItemSpawn")->at(0);
+	repair_spawns_ = QUADTREE->GetGuideLines("DND_RepairPart_1")->at(0);
 }
 
 void reality::WaveSystem::OnUpdate(entt::registry& reg)
@@ -30,13 +34,14 @@ void reality::WaveSystem::OnUpdate(entt::registry& reg)
 	}
 
 	countdown_timer_ -= TM_DELTATIME;
+	PlayerExtractRepair();
 }
 
 void reality::WaveSystem::SetWorldEnv(Environment* env)
 {
 	world_env_ = shared_ptr<Environment>(env);
 	countdown_timer_ = fabs(world_env_->GetTimeLimits().x * 2);
-	for (const auto& node : item_spawns_->at(0).line_nodes)
+	for (const auto& node : item_spawns_.line_nodes)
 	{
 		item_table_.insert(make_pair(node.first, false));
 	}
@@ -57,8 +62,97 @@ void reality::WaveSystem::RandomSpawnItem(float trigger_radius)
 	{
 		ItemType item_type = (ItemType)(rand() % 7);
 
-		const auto& spawn = item_spawns_->at(0).line_nodes[index];
+		const auto& spawn = item_spawns_.line_nodes[index];
 		SCENE_MGR->AddActor<Item>(item_type, _XMFLOAT3(spawn), trigger_radius);
 		item_table_[index] = true;
+	}
+}
+
+void reality::WaveSystem::CreateExtractPoints(entt::registry& reg)
+{
+	srand(time(NULL));
+	UINT repair_part_count = 5;
+	set<UINT> random_indices;
+	for (int i = 0; i < repair_part_count; ++i)
+	{
+		UINT index = rand() % repair_spawns_.line_nodes.size();
+
+		if (random_indices.find(index) == random_indices.end())
+			random_indices.insert(index);
+		else
+			i -= 1;
+	} 
+
+	for (const auto& node : repair_spawns_.line_nodes)
+	{
+		C_TriggerVolume trigger;
+		trigger.tag = "extract";
+
+		trigger.sphere_volume.center = _XMFLOAT3(node.second);
+		trigger.sphere_volume.radius = 100;
+
+		entt::entity trigger_entity = reg.create();
+		reg.emplace<C_TriggerVolume>(trigger_entity, trigger);
+
+		
+		if (random_indices.find(node.first) != random_indices.end())
+			repair_parts.insert(trigger_entity);
+	}
+}
+
+void reality::WaveSystem::PlayerExtractRepair()
+{
+	auto player = SCENE_MGR->GetPlayer<Player>(0);
+	if (player == nullptr)
+		return;
+
+	if (player->can_extract_repair == false)
+		return;
+
+	static float extract_time = 0.0f;
+
+	if (DINPUT->GetKeyState(DIK_E) == KEY_HOLD)
+		extract_time += TM_DELTATIME;
+
+	if (DINPUT->GetKeyState(DIK_E) == KEY_UP)
+		extract_time = 0.0f;
+
+	if (extract_time >= player->extract_time_takes)
+	{
+		if (repair_parts.find(player->repair_extract_trigger) != repair_parts.end())
+		{
+			repair_parts.erase(player->repair_extract_trigger);
+
+			auto trigger_comp = SCENE_MGR->GetScene(INGAME)->GetRegistryRef().try_get<C_TriggerVolume>(player->repair_extract_trigger);
+			if (trigger_comp == nullptr)
+				return;
+
+			SpawnRepairItem(trigger_comp->sphere_volume.center);
+		}
+		else
+		{
+			// extract complete but there was no repair item
+
+		}
+		DeleteExtractPoint(player->repair_extract_trigger);
+		extract_time = 0.0f;
+		player->can_extract_repair = false;
+	}
+}
+
+void reality::WaveSystem::SpawnRepairItem(XMFLOAT3 position)
+{
+	SCENE_MGR->AddActor<Item>(ItemType::eRepairPart, position, 30.0f);
+}
+
+void reality::WaveSystem::DeleteExtractPoint(entt::entity ent)
+{
+	auto player = SCENE_MGR->GetPlayer<Player>(0);
+	if (player == nullptr)
+		return;
+
+	if (SCENE_MGR->GetScene(INGAME)->GetRegistryRef().valid(ent))
+	{
+		SCENE_MGR->GetScene(INGAME)->GetRegistryRef().destroy(ent);
 	}
 }
