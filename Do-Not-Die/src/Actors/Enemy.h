7a7f475d024602a553e9d48c1bd738549f3c4567
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-04-20 21:08:14 +0900

    feat: 적의 애니메이션 상태 머신 수정

--- Do-Not-Die/src/Actors/Enemy.h
+++ Do-Not-Die/src/Actors/Enemy.h
@@ -109,6 +109,16 @@ namespace reality {
 					}
 				})
 				});
+			transitions_.insert({ MOVE, Transition(HIT,[this](const AnimationStateMachine* animation_state_machine) {
+					Enemy* enemy = SCENE_MGR->GetActor<Enemy>(owner_id_);
+					if (enemy->is_hit_ && enemy->GetCurHp() >= 0.) {
+						return true;
+					}
+					else {
+						return false;
+					}
+				})
+				});
 			transitions_.insert({ IDLE, Transition(HIT,[this](const AnimationStateMachine* animation_state_machine) {
 					Enemy* enemy = SCENE_MGR->GetActor<Enemy>(owner_id_);
 					if (enemy->is_hit_) {
@@ -139,9 +149,19 @@ namespace reality {
 					}
 				})
 				});
-			transitions_.insert({ MOVE, Transition(HIT,[this](const AnimationStateMachine* animation_state_machine) {
+			transitions_.insert({ MOVE, Transition(DIE,[this](const AnimationStateMachine* animation_state_machine) {
 					Enemy* enemy = SCENE_MGR->GetActor<Enemy>(owner_id_);
-					if (enemy->is_hit_) {
+					if (enemy->is_hit_ == true && enemy->GetCurHp() <= 0) {
+						return true;
+					}
+					else {
+						return false;
+					}
+				})
+				});
+			transitions_.insert({ IDLE, Transition(DIE,[this](const AnimationStateMachine* animation_state_machine) {
+					Enemy* enemy = SCENE_MGR->GetActor<Enemy>(owner_id_);
+					if (enemy->is_hit_ == true && enemy->GetCurHp() <= 0) {
 						return true;
 					}
 					else {
@@ -150,16 +170,6 @@ namespace reality {
 				})
 				});
 
-			transitions_.insert({ HIT, Transition(DIE,[this](const AnimationStateMachine* animation_state_machine) {
-					Enemy* enemy = SCENE_MGR->GetActor<Enemy>(owner_id_);
-					if (IsAnimationEnded() == true && enemy->GetCurHp() <= 0) {
-						return true;
-					}
-					else {
-						return false;
-					}
-				})
-				});
 			transitions_.insert({ DIE, Transition(IDLE,[this](const AnimationStateMachine* animation_state_machine) {
 					Enemy* enemy = SCENE_MGR->GetActor<Enemy>(owner_id_);
 					if (IsAnimationEnded() == true) {
@@ -201,7 +211,7 @@ namespace reality {
 		public:
 			virtual void Enter(AnimationStateMachine* animation_state_machine) override
 			{
-				animation_state_machine->SetAnimation("Zombie_Walk_F_6_Loop_IPC_Anim_Unreal Take.anim", 0.2f);
+				animation_state_machine->SetAnimation("Zombie_Walk_F_6_Loop_IPC_Anim_Unreal Take.anim", 0.8f);
 			}
 			virtual void Exit(AnimationStateMachine* animation_state_machine) override
 			{
@@ -217,7 +227,7 @@ namespace reality {
 		public:
 			virtual void Enter(AnimationStateMachine* animation_state_machine) override
 			{
-				animation_state_machine->SetAnimation("Zombie_Atk_KnockBack_1_IPC_Anim_Unreal Take.anim", 0.3f);
+				animation_state_machine->SetAnimation("Zombie_Atk_KnockBack_1_IPC_Anim_Unreal Take.anim", 0.8f);
 			}
 			virtual void Exit(AnimationStateMachine* animation_state_machine) override
 			{
@@ -225,6 +235,8 @@ namespace reality {
 			}
 			virtual void OnUpdate(AnimationStateMachine* animation_state_machine) override
 			{
+				Enemy* enemy = SCENE_MGR->GetActor<Enemy>(animation_state_machine->GetOwnerId());
+				enemy->CancelMovement();
 			}
 		};
 
@@ -234,7 +246,7 @@ namespace reality {
 		public:
 			virtual void Enter(AnimationStateMachine* animation_state_machine) override
 			{
-				animation_state_machine->SetAnimation("Zombie_Death_Back_Mid_1_IPC_Anim_Unreal Take.anim", 0.3f);
+				animation_state_machine->SetAnimation("Zombie_Death_Back_Mid_1_IPC_Anim_Unreal Take.anim", 0.5f);
 			}
 			virtual void Exit(AnimationStateMachine* animation_state_machine) override
 			{
@@ -242,6 +254,8 @@ namespace reality {
 			}
 			virtual void OnUpdate(AnimationStateMachine* animation_state_machine) override
 			{
+				Enemy* enemy = SCENE_MGR->GetActor<Enemy>(animation_state_machine->GetOwnerId());
+				enemy->CancelMovement();
 			}
 		};
 	};
