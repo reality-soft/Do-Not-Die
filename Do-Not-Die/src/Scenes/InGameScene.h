d61f4f6f6c1ce1e6554c64e8f2c5cea7b08b4e34
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-04-20 18:09:36 +0900

    feat : WaveSystem에서 좀비 스폰

--- Do-Not-Die/src/Scenes/InGameScene.h
+++ Do-Not-Die/src/Scenes/InGameScene.h
@@ -27,16 +27,6 @@ public:
 	virtual void OnRelease();
 
 private:
-	vector<std::string> enemy_meshes = {
-		"Zombie_Businessman_Male_01.skmesh",
-		"Zombie_Cheerleader_Female_01.skmesh",
-		"Zombie_Daughter_Female_01.skmesh",
-		"Zombie_Police_Male_01.skmesh",
-		"Zombie_RiotCop_Male_01.skmesh",
-		"Zombie_SchoolGirl_Female_01.skmesh",
-		"Zombie_Bellboy_Male_01.skmesh",
-	};
-
 	Environment environment_;
 	StaticMeshLevel level;
 	//SingleShadow single_shadow;
