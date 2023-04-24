b49de18ba6b13c5ace08b5f4b1e27b48e7bc2bc8
EussyKR <hang3137@naver.com>
2023-04-24 21:07:16 +0900

    feat : Start Scene에 로딩 단계 추가

--- Do-Not-Die/src/Scenes/StartScene.cpp
+++ Do-Not-Die/src/Scenes/StartScene.cpp
@@ -3,16 +3,21 @@
 
 void StartScene::OnInit()
 {
+	loading_progress = START_RESOURCE;
 	reality::RESOURCE->Init("../../Contents/");
 
-	WRITER->Init();
-	reality::ComponentSystem::GetInst()->OnInit(reg_scene_);
-	
+	loading_progress = START_MANAGER;
+	COMPONENT->OnInit(reg_scene_);
+
+	loading_progress = START_SYSTEM;
 	sys_render.OnCreate(reg_scene_);
 	sys_ui.OnCreate(reg_scene_);
 	sys_sound.OnCreate(reg_scene_);
 
+	loading_progress = START_ACTOR;
 	start_scene_ui.OnInit(reg_scene_);
+
+	loading_progress = START_FINISHED;
 }
 
 void StartScene::OnUpdate()
