e2ddcffa04e5c6175e1cfbc80929f39c02538440
EussyKR <hang3137@naver.com>
2023-04-25 15:06:27 +0900

    feat : 로딩 씬 제거 후 인트로 씬 추가

---
+++ Do-Not-Die/src/UI/IntroScene/UI_Actor_IntroScene.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#include "UI_Actor_IntroScene.h"
+#include "Engine.h"
+#include "SceneMgr.h"
+
+using namespace reality;
+
+void UI_Actor_IntroScene::OnInit(entt::registry& registry)
+{
+	UIActor::OnInit(registry);
+
+	CreateUI();
+
+}
+
+void UI_Actor_IntroScene::OnUpdate()
+{
+	UpdateUI();
+}
+
+void UI_Actor_IntroScene::CreateUI()
+{
+	C_UI& ui_comp = reg_scene_->get<C_UI>(GetEntityId());
+
+	float win_size_1920_width = E_Resolution_Size[E_Resolution::R1920x1080].x;
+	float win_size_1920_height = E_Resolution_Size[E_Resolution::R1920x1080].y;
+
+
+	// Background UI
+	background_ = make_shared<UI_Image>();
+	background_->InitImage("T_Intro_Background.png");
+	background_->SetLocalRectByMin({ 0, 0 }, win_size_1920_width, win_size_1920_height);
+
+	intro_logo_ = make_shared<UI_Image>();
+	intro_logo_->InitImage("T_IntroLogo.png");
+	background_->AddChildUI("1_Intro_Logo", intro_logo_);
+	intro_logo_->SetLocalRectByMin({ 0, 0 }, win_size_1920_width, win_size_1920_height);
+	
+	ui_comp.ui_list.insert({ "Background", background_ });
+}
+
+void UI_Actor_IntroScene::UpdateUI()
+{
+	intro_timer_ += TIMER->GetDeltaTime();
+
+	if (intro_timer_ < intro_time_ / 2.0f)
+		intro_logo_->SetAlpha(intro_timer_ / (intro_time_ / 2.0f));
+	else if (intro_timer_ > intro_time_ / 2.0f && intro_timer_ < intro_time_)
+		intro_logo_->SetAlpha((intro_time_ - (intro_timer_ - intro_time_ / 2.0f)) / (intro_time_ / 2.0f));
+}
+
