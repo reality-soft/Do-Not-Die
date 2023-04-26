8413120dba270aea2a169b31849d0a2f6285c392
EussyKR <hang3137@naver.com>
2023-04-26 15:08:08 +0900

    chore : 이펙트 포그 알파값 수정

--- Do-Not-Die/HLSL/EffectDefaultPS.hlsl
+++ Do-Not-Die/HLSL/EffectDefaultPS.hlsl
@@ -6,6 +6,6 @@ float4 PS(PS_IN input) : SV_Target
 	float4 tex_color = g_txDiffuse.Sample(g_SampleWrap, input.t);
 	float4 final_color = tex_color * input.c;
 	final_color = ApplyDistanceFog(final_color, input.world_pos);
-	//final_color.a *= input.c.a;
+	final_color.a *= tex_color.a;
 	return final_color;
 }
