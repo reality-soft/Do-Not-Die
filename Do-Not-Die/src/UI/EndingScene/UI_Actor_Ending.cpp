1cee1781aba7add863b08cb75b9c835fac1f9a4d
EussyKR <hang3137@naver.com>
2023-05-02 13:56:45 +0900

    feat : 크레딧 스크롤 함수 추가

--- Do-Not-Die/src/UI/EndingScene/UI_Actor_Ending.cpp
+++ Do-Not-Die/src/UI/EndingScene/UI_Actor_Ending.cpp
@@ -10,8 +10,19 @@ void UI_Actor_Ending::OnInit(entt::registry& registry)
 	float win_size_1920_height = E_Resolution_Size[E_Resolution::R1920x1080].y;
 
 	// Ending Credits
+	string ending_credit_tex_id = "T_EndingCredits.png";
+	auto tex = RESOURCE->UseResource<Texture>(ending_credit_tex_id);
+	ending_credit_size_ = { (int)tex->texture_desc.Width, (int)tex->texture_desc.Height };
 
-
+	ending_credits_ = make_shared<UI_Image>();
+	ending_credits_->InitImage(ending_credit_tex_id);
+	ending_credits_->SetLocalRectByMin({ 0.0f, 0.0f }, win_size_1920_width, win_size_1920_height);
+	ending_credits_->render_data_.vertex_list[0].t = { 0.0f, 0.0f };
+	ending_credits_->render_data_.vertex_list[1].t = { 1.0f, 0.0f };
+	ending_credits_->render_data_.vertex_list[2].t = { 0.0f, 1080.0f / ending_credit_size_.y };
+	ending_credits_->render_data_.vertex_list[3].t = { 1.0f, 1080.0f / ending_credit_size_.y };
+	ending_credits_->UpdateRenderCB();
+	ui_comp.ui_list.insert({ "A_EndingCredits", ending_credits_ });
 
 	// Fade BG
 	ui_fade_background_ = make_shared<UI_Image>();
@@ -28,6 +39,8 @@ void UI_Actor_Ending::OnUpdate()
 
 	if (FadeOutBg() == true)
 		timer += TM_DELTATIME;
+
+	ScrollCredit(timer, 15.0f);
 
 	if (timer > 15.0f)
 		ending_finished = FadeInBg();
@@ -64,4 +77,17 @@ bool UI_Actor_Ending::FadeInBg()
 
 	ui_fade_background_->SetAlpha(alpha);
 	return false;
+}
+
+void UI_Actor_Ending::ScrollCredit(float cur_timer, float end_time)
+{
+	float y = ending_credit_size_.y - 1080.0f;
+	float top_uv = (y * (cur_timer / end_time)) / ending_credit_size_.y;
+	float bottom_uv = (1080.0f + y * (cur_timer / end_time)) / ending_credit_size_.y;
+
+	ending_credits_->render_data_.vertex_list[0].t = { 0.0f, top_uv };
+	ending_credits_->render_data_.vertex_list[1].t = { 1.0f, top_uv };
+	ending_credits_->render_data_.vertex_list[2].t = { 0.0f, bottom_uv };
+	ending_credits_->render_data_.vertex_list[3].t = { 1.0f, bottom_uv };
+	ending_credits_->UpdateRenderCB();
 }
--- Do-Not-Die/src/UI/EndingScene/UI_Actor_Ending.h
+++ Do-Not-Die/src/UI/EndingScene/UI_Actor_Ending.h
@@ -12,8 +12,11 @@ public:
 
 	bool FadeOutBg();
 	bool FadeInBg();
+	void ScrollCredit(float cur_timer, float end_time);
 
 	// Game Over UI
+	XMINT2 ending_credit_size_;
+	shared_ptr<UI_Image> ending_credits_;
 	shared_ptr<UI_Image> ui_fade_background_;
 	bool ending_finished = false;
 };
